ccf3d0fbe0441d16a73e9d0a5904c244553a67c9
bbbottle <z@zjh.im>
2024-10-05 16:00:35 +0800

    chore: remove store

--- src/bot/middlewares/session/index.ts
+++ src/bot/middlewares/session/index.ts
@@ -10,15 +10,15 @@ export interface BBSession {
   SupabaseUser?: User,
 }
 
-const store = Postgres<BBSession>({
-  host: process.env.PSQL_HOST as string,
-  database: process.env.PSQL_DB as string,
-  user: process.env.PSQL_USER as string,
-  password: process.env.PSQL_PASS as string,
-});
+// const store = Postgres<BBSession>({
+//   host: process.env.PSQL_HOST as string,
+//   database: process.env.PSQL_DB as string,
+//   user: process.env.PSQL_USER as string,
+//   password: process.env.PSQL_PASS as string,
+// });
 
-export const SessionMiddleware = session<BBSession, BBContext, "session">({
-  store: store as SessionStore<BBSession>,
+export const indexSessionMiddleware = session<BBSession, BBContext, "session">({
+  // store: store as SessionStore<BBSession>,
 });
 
 export const SessionRestore = Composer.optional(HasLogin, async (ctx, next) => {
